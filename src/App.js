148279582e27cd49b81272d7a1f23abf0f6e1ea2
Ljupco1982 <54242904+Ljupco1982@users.noreply.github.com>
2020-04-17 16:20:35 +0200

    broken our homepage into smaller components, finished styling our manu-item component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import './App.css';
 
-import  HomePage from './homepage.component';
+import HomePage from './pages/homepage.component';
 
 function App() {
   return (
     <div>
-    < HomePage />
+      <HomePage />
     </div>
   );
 }
