034f41e5c6806b0cb66cf9b4250345b4c1bac320
Ljupco1982 <54242904+Ljupco1982@users.noreply.github.com>
2020-04-25 11:00:25 +0200

    Updates

---
+++ precache-manifest.e2ec46727e41c1d0792b5458ee25e4a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266417325ca6c7151836d838ceae2b66",
+    "url": "/Ljupco1982/crwn-clothing/index.html"
+  },
+  {
+    "revision": "bd20ee1ef0401b4b098b",
+    "url": "/Ljupco1982/crwn-clothing/static/css/main.b41b025c.chunk.css"
+  },
+  {
+    "revision": "9d8a287aed1b380a92be",
+    "url": "/Ljupco1982/crwn-clothing/static/js/2.d29d51aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Ljupco1982/crwn-clothing/static/js/2.d29d51aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd20ee1ef0401b4b098b",
+    "url": "/Ljupco1982/crwn-clothing/static/js/main.d78d81ca.chunk.js"
+  },
+  {
+    "revision": "c9fe650310e97ba0c23d",
+    "url": "/Ljupco1982/crwn-clothing/static/js/runtime-main.b411a20c.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/Ljupco1982/crwn-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
